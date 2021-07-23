714ddffb89f6d57fe807aaa3eef6f1c0b00fb18c
inz2020 <>
2021-07-23 13:42:30 +0200

    creation du composant Search.js

--- App.js
+++ App.js
@@ -3,10 +3,13 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import About from './components/About'
+import About from './components/Search'
 
 export default function App() {
   return (
-    <About/>
+    <View style={{marginVertical:20}}>
+    <Search/>
+    <View/>
   );
 }
 
--- components/Search.js
+++ components/Search.js
@@ -1,9 +1,27 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, TextInput} from 'react-native'
 
 
-export default class Home extends React.Component{
+export default class  Search extends React.Component{
+const [text, onChangeText] = React.useState("Useless Text");
 render(){
 return(
+  <TextInput
+         style={styles.input}
+         onChangeText={onChangeText}
+         value={text}
+         placeholder="Entrer la ville"
+         keyboardType="text"
+         underlineColorAndroid='transparent'
+       />
 )}
 }
+
+const styles = StyleSheet.create({
+  input: {
+   height: 40,
+    borderColor: 'gray',
+    borderWith: '1',
+    justifyContent: 'center',
+  },
+});
